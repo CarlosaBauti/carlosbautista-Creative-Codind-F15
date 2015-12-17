471bf2376b11e9c7940b19746da8f28ad9c4f8fc
Carlos Bautista <cbi210@nyu.edu>
2015-12-17 11:51:16 -0500

    one homework two iterations

---
+++ homework_08/rectangles_function/rectangles_iteration_01/sketch_iteration_01.js
@@ -0,0 +1,41 @@
+
+function setup(){
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw(){
+  frameRate(15)
+  background(255);
+  drawRect(windowHeight*2,windowWidth*2);
+}
+
+function drawRect(ancho,alto){
+  if(alto>10){
+    alto *=0.5;
+    console.log(alto);
+    // ancho*=0.5;
+    theta=PI/2;
+    push();
+    translate(alto,0);
+    console.log(alto);
+    rotate(theta);
+    stroke(200);
+    fill(random(50),random(255),random(220));
+    rect (0,0,ancho,alto);
+    // line(0,0,40,40);
+    // drawRect(alto,ancho);
+    pop();
+    
+    push();
+    translate(alto/2,0);
+    console.log(alto);
+    rotate(theta);
+    stroke(255);
+    fill(random(255),5,random(252));
+    rect (0,0,ancho,alto/2);
+    // line(0,0,40,40);
+    drawRect(alto,ancho);
+    pop();
+    
+  }
+};
